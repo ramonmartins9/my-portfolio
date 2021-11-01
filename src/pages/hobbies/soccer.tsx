e26bb990e51f6d01d9505a944fda4a9969dba579
Ramon Martins <47434666+ramonmartins9@users.noreply.github.com>
2021-11-01 18:45:08 -0300

    Update soccer.tsx

--- src/pages/hobbies/soccer.tsx
+++ src/pages/hobbies/soccer.tsx
@@ -11,9 +11,6 @@ const Soccer: NextPage = () => (
     	<PageContainer pageTitle={strings.link.hobbies} workTitle={strings.hobbies.soccer.title}>
 			<Text mt={2} fontSize="md" >{strings.hobbies.soccer.stanza}</Text>
 			<Box borderRadius="lg" overflow="hidden">
-				<Image quality={100} layout="responsive" width="80%" height="100%" src="/futebolme.jpeg" />
-			</Box>
-			<Box borderRadius="lg" overflow="hidden">
 				<Image quality={100} layout="responsive" width="100%" height="50%" src="/soccertean.png" />
 			</Box>
 	  	</PageContainer>
